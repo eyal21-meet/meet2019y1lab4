68d609325ab138d79034aa5ced0275b3fda13b70
eyal21-meet <eyal21@meet.mit.edu>
2019-07-24 09:57:38 -0400

    Stopping here

---
+++ fruit detector.py
@@ -0,0 +1,9 @@
+fruit = input("choose a fruit: ")
+if fruit == "apples":
+    print("bin 1")
+elif fruit == "oranges":
+    print("bin 2")
+elif fruit == "bananas":
+    print("bin 3")
+else:
+    print("Error! I do not recognize this fruit!")
